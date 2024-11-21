63a45945b93f7d62be2db001667c3099ba4c10fa
Joshua-Production <joshuaespinosabusiness@outlook.com>
2024-11-21 13:12:08 -0600

    Added controls to the starting screen

--- MathForGamesDemo/src/Engine/StartScene.cs
+++ MathForGamesDemo/src/Engine/StartScene.cs
@@ -25,9 +25,20 @@ namespace MathForGamesDemo
                 Game.CurrentScene = Game.GetScene(1);
             }
 
-            Raylib.DrawText(" Welcome to Cross Fire", 55, 50, 35, Color.Black );
-            Raylib.DrawText(" Your goal is to get to the finish line ",  35, 100, 27, Color.Black );
-          
+            Raylib.DrawText(" Welcome to Cross Fire", 20, 50, 40, Color.Black);
+            Raylib.DrawText(" Your goal is to get to the finish line ", 35, 90, 27, Color.Black);
+            Raylib.DrawText(" The part of your tank with the dark blue line is the back ", 1, 150, 19, Color.Black);
+            Raylib.DrawText(" CONTROLS ", 100, 210, 55, Color.Black);
+            Raylib.DrawText(" W is forwards ", 120, 290, 40, Color.Black);
+            Raylib.DrawText(" S is backwards", 120, 340, 40, Color.Black);
+            Raylib.DrawText(" A is Rotate the bottom of your tank to the left", 20, 410, 20, Color.Black);
+            Raylib.DrawText(" D is Rotate the bottom of your tank to the right", 20, 430, 20, Color.Black);
+            Raylib.DrawText(" Left Arrow rotates your turret to the left", 30, 460, 20, Color.Black);
+            Raylib.DrawText(" Right Arrow rotates your turret to the right", 27, 480, 20, Color.Black);
+            Raylib.DrawText(" Spacebar is shoot", 70, 520, 40, Color.Black);
+
+            Raylib.DrawText(" Press enter to start ", 1, 800, 48, Color.Black);
+
 
 
 
